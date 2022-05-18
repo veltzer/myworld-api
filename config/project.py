069db873d6f86df028d1bb558630fb59a86d6598
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:01 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "myworld-api"
-project_long_description = "myworld system as API"
-project_year_started = 2016
-project_description = """myworld system as API"""
+description_short = "myworld system as API"
